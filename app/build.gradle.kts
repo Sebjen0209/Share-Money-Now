7a13ccc44592175c129c00b0de72e64cc81e5125
CasperKirud <ckirud@gmail.com>
2023-11-23 19:56:17 +0100

    Revert "updated gradle"
    This reverts commit cd65ab7fb0bbb23ad9661c634c17cf23e55e1d18.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.share_money_now"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.share_money_now"
