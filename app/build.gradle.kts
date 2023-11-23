421dbade092d94c7c28ebb1990d782763bde98c3
Sebjen0209 <70900449+Sebjen0209@users.noreply.github.com>
2023-11-23 19:53:22 +0100

    change compileSdk in build.gradle.kts to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.share_money_now"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.share_money_now"
