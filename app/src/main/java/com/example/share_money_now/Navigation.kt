f04d91b0ce4398f56633f49cc6b4bf7614256e2a
CasperKirud <ckirud@gmail.com>
2023-11-24 15:57:09 +0100

    Able to create a new group and it will show up in the list

--- app/src/main/java/com/example/share_money_now/Navigation.kt
+++ app/src/main/java/com/example/share_money_now/Navigation.kt
@@ -8,7 +8,7 @@ import androidx.navigation.compose.rememberNavController
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.UserScreen.route) {
+    NavHost(navController = navController, startDestination = Screen.LandingScreen.route) {
         composable(route = Screen.WelcomePage.route){
             WelcomePage(navController = navController)
         }
