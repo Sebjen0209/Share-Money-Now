4de8a938445acd2cfbb033c2b543ba7b1c480e84
CasperKirud <ckirud@gmail.com>
2023-11-26 13:07:01 +0100

    added dialogscreen when pressing add people

--- app/src/main/java/com/example/share_money_now/GroupScreen.kt
+++ app/src/main/java/com/example/share_money_now/GroupScreen.kt
@@ -6,27 +6,38 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun GroupScreen(navController: NavController) {
     var groupName by remember { mutableStateOf("Group Name") }
     var totalAmount by remember { mutableStateOf(1000.0) }
     var participants by remember { mutableStateOf(listOf("Participant 1", "Participant 2")) }
     var description by remember { mutableStateOf("Group Description") }
+    var showDialog by remember { mutableStateOf(false) }
+    var textFieldValue by remember { mutableStateOf(TextFieldValue()) }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Scaffold(
         topBar = {
@@ -87,13 +98,96 @@ fun GroupScreen(navController: NavController) {
 
             // Add People Button
             Button(
-                onClick = { /* Handle add people button click */ },
+                onClick = {
+                    showDialog = true
+                    keyboardController?.show()
+                          },
                 modifier = Modifier
                     .fillMaxWidth()
                     .background(MaterialTheme.colorScheme.secondary)
                     .padding(8.dp)
             ) {
                 Text(text = "Add People")
+            }
+
+            // Dialog for adding people
+            if (showDialog) {
+                Dialog(
+                    onDismissRequest = {
+                        showDialog = false
+                        // Hide the keyboard when the dialog is dismissed
+                        keyboardController?.hide()
+                    },
+                    content = {
+                        Column(
+                            modifier = Modifier
+                                .padding(16.dp)
+                        ) {
+                            Text(
+                                text = "Add People",
+                                fontSize = 20.sp,
+                                modifier = Modifier
+                                    .padding(bottom = 8.dp)
+                            )
+
+                            TextField(
+                                value = textFieldValue,
+                                onValueChange = { newValue ->
+                                    textFieldValue = newValue
+                                },
+                                label = { Text("Enter email of person") },
+                                keyboardOptions = KeyboardOptions.Default.copy(
+                                    imeAction = ImeAction.Done
+                                ),
+                                keyboardActions = KeyboardActions(
+                                    onDone = {
+                                        showDialog = false
+                                        // Hide the keyboard when the "Done" action is triggered
+                                        keyboardController?.hide()
+                                    }
+                                ),
+                                colors = TextFieldDefaults.textFieldColors(
+                                    focusedIndicatorColor = Color.Transparent,
+                                    unfocusedIndicatorColor = Color.Transparent,
+                                    cursorColor = contentColorFor(MaterialTheme.colorScheme.background)
+                                ),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp)
+                            )
+
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 16.dp),
+                                horizontalArrangement = Arrangement.End
+                            ) {
+                                TextButton(
+                                    onClick = {
+                                        showDialog = false
+                                        // Hide the keyboard when the "Cancel" button is clicked
+                                        keyboardController?.hide()
+                                    }
+                                ) {
+                                    Text(text = "Cancel")
+                                }
+
+                                Spacer(modifier = Modifier.width(16.dp))
+
+                                TextButton(
+                                    onClick = {
+                                        // Perform the action of adding people (e.g., save to Firebase)
+                                        showDialog = false
+                                        // Hide the keyboard when the "Add" button is clicked
+                                        keyboardController?.hide()
+                                    }
+                                ) {
+                                    Text(text = "Add")
+                                }
+                            }
+                        }
+                    }
+                )
             }
 
             // Pay Button
